9e85045ea1b17b6348882cbe3495f07a977c668b
daelsaid <daelsaid@stanford.edu>
2019-09-24 17:51:45 -0700

    renamed dir

--- ancova_group_by_time/archive/anova_batch_with_corr_design_matrix_job.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'/oak/stanford/groups/menon/projects/daelsaid/2019_mathfun/scripts/taskfmri/groupstats/anova_group_by_time/anova_batch_with_corr_design_matrix_job_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
--- ancova_group_by_time/example/addition_pre_post_GroupByTime.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'/oak/stanford/groups/menon/projects/lchen32/2018_MathFUN_mindset/scripts/taskfmri/groupstats/Pre_Post/Group_by_Time/addition_pre_post_Tut_GroupByTime_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
